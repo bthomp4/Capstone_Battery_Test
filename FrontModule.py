650ebe737179c6c1a08cb8e32b63941e808c872b
Emily <elambert423@gmail.com>
2018-02-14 17:20:51 -0500

    Added Front Module
    Added front module and took some processes out of rear module and added it to the front module

---
+++ FrontModule.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#
+# FrontModule.py
+# front module code that receives images and distances from the rear module.
+# will display the new image and will light up leds depending on the distances
+#
+# Green Status LED in the bottom right hand corner. If properly connected stay lit up
+# other wise blink
+# 1 LED for every 10 feet in front of you.
+#
+# Ultrasonic related posts:
+# http://www.raspberrypi-spy.co.uk/tag/ultrasonic/
+#
+# Author : Emily Lambert
+# Date     : 02/14/2018
+# -----------------------
+
+# Python Libraries
+import os
+import RPi.GPIO as GPIO
+from PIL import Image
+import time
+from multiprocessing import Process
+from random import randint  # delete this later
+
+# GPIO pins and their purpose
+def GPIO_TRIGGER           = 23    # Trigger for ultrasonic sensor
+def GPIO_ECHO              = 20    # Echo for ultrasonic sensor
+def GPIO_LEDSRIGHT         = 21    # LEDS will light up at same time so one GPIO is needed
+def GPIO_LEDSLEFT          = 27    # LEDS will light up at same time so one GPIO is needed
+def GPIO_FRONTLED1         = 2
+def GPIO_FRONTLED2         = 3
+def GPIO_FRONTLED3         = 4
+def GPIO_FRONTLED4         = 17
+def GPIO_FRONTLED5         = 27
+def GPIO_FRONTLED6         = 22
+def GPIO_FRONTLED7         = 10
+def GPIO_FRONTLED8         = 9
+def GPIO_FRONTLED9         = 11
+def GPIO_FRONTLED10        = 0
+
+# ---------------------------------------------------
+# measure takes a measurement from the front sensor
+# ---------------------------------------------------
+def measure():
+    # This function measures a distance
+    GPIO.output(GPIO_TRIGGER, True)
+    # Wait 10us
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+    
+    while GPIO.input(GPIO_ECHO)==0:
+        start = time.time()
+
+    while GPIO.input(GPIO_ECHO)==1:
+        stop = time.time()
+
+    elapsed = stop-start
+    distance = (elapsed * speedSound)/2
+
+    return distance
+
+# ---------------------------------------------------
+# measure_average finds the average of 3 measurements
+# ---------------------------------------------------
+def measure_average():
+    # This function takes 3 measurements and
+    # returns the average.
+
+    distance1=measure()
+    time.sleep(0.1)
+    distance2=measure()
+    time.sleep(0.1)
+    distance3=measure()
+    distance = distance1 + distance2 + distance3
+    distance = distance / 3
+    return distance
+
+def pictureDisplay():
+    for i in range(4200):   #run for ~10 minutes?
+        sleepTime = 0.5 # sleep time in seconds
+        # Get picture message
+        os.system("bash -c 'ping -w 1 umbc.edu'")
+        # Display picture
+        image = open('test.jpg','wb')
+        image_read = image.read()
+        time.sleep(sleepTime)
+
+def updateLEDs():
+    # Use BCM GPIO references
+    # instead of physical pin numbers
+    GPIO.setmode(GPIO.BCM)
+
+    print("Ultrasonic Measurement")
+
+    # Set pins as output and input 
+    GPIO.setup(GPIO_TRIGGER,GPIO.OUT)    # Trigger 1
+    GPIO.setup(GPIO_ECHO,GPIO.IN)        # Echo 1
+    GPIO.setup(GPIO_LEDSRIGHT,GPIO.OUT)
+    GPIO.setup(GPIO_LEDSLEFT,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED1,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED2,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED3,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED4,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED5,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED6,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED7,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED8,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED9,GPIO.OUT)
+    GPIO.setup(GPIO_FRONTLED10,GPIO.OUT)
+
+    # Set trigger to False (Low)
+    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(GPIO_LEDSRIGHT, False)
+    GPIO.output(GPIO_LEDSLEFT, False)
+    GPIO.output(GPIO_FRONTLED1, False)
+    GPIO.output(GPIO_FRONTLED2, False)
+    GPIO.output(GPIO_FRONTLED3, False)
+    GPIO.output(GPIO_FRONTLED4, False)
+    GPIO.output(GPIO_FRONTLED5, False)
+    GPIO.output(GPIO_FRONTLED6, False)
+    GPIO.output(GPIO_FRONTLED7, False)
+    GPIO.output(GPIO_FRONTLED8, False)
+    GPIO.output(GPIO_FRONTLED9, False)
+    GPIO.output(GPIO_FRONTLED10, False)
+
+    # Allow module to settle
+    time.sleep(0.5)
+
+    # Wrap main content in a try block so we can
+    # catch the user pressing CTRL-C and run the
+    # GPIO cleanup function. This will also prevent
+    # the user seeing lots of unnecessary error
+    # messages.
+    try:
+    print("Distance Recorded at Front Sensor") 
+    while True:
+        
+        # Get Distance data from front sensor
+        distance = measure_average()
+    
+        print(int(distance), "inches\n")
+        
+        # Turn on front LEDs as needed
+        if distance > 100:
+            GPIO.output(GPIO_FRONTLED1, False)
+            GPIO.output(GPIO_FRONTLED2, False)
+            GPIO.output(GPIO_FRONTLED3, False)
+            GPIO.output(GPIO_FRONTLED4, False)
+            GPIO.output(GPIO_FRONTLED5, False)
+            GPIO.output(GPIO_FRONTLED6, False)
+            GPIO.output(GPIO_FRONTLED7, False)
+            GPIO.output(GPIO_FRONTLED8, False)
+            GPIO.output(GPIO_FRONTLED9, False)
+            GPIO.output(GPIO_FRONTLED10, False)
+        if distance <= 1200:
+            GPIO.output(GPIO_FRONTLED1, True)
+        else:
+            GPIO.output(GPIO_FRONTLED1, False)
+        if distance <= 1080:
+            GPIO.output(GPIO_FRONTLED2, True)
+        else:
+            GPIO.output(GPIO_FRONTLED2, False)
+        if distance <= 960:
+            GPIO.output(GPIO_FRONTLED3, True)
+        else:
+            GPIO.output(GPIO_FRONTLED3, False)
+        if distance <= 840:
+            GPIO.output(GPIO_FRONTLED4, True)
+        else:
+            GPIO.output(GPIO_FRONTLED4, False)
+        if distance <= 720:
+            GPIO.output(GPIO_FRONTLED5, True)
+        else:
+            GPIO.output(GPIO_FRONTLED5, False)
+        if distance <= 600:
+            GPIO.output(GPIO_FRONTLED6, True)
+        else:
+            GPIO.output(GPIO_FRONTLED6, False)
+        if distance <= 480:
+            GPIO.output(GPIO_FRONTLED7, True)
+        else:
+            GPIO.output(GPIO_FRONTLED7, False)
+        if distance <= 360:
+            GPIO.output(GPIO_FRONTLED8, True)
+        else:
+            GPIO.output(GPIO_FRONTLED8, False)
+        if distance <= 240:
+            GPIO.output(GPIO_FRONTLED9, True)
+        else:
+            GPIO.output(GPIO_FRONTLED9, False)  
+        if distance <= 120:
+            GPIO.output(GPIO_FRONTLED10, True)
+        else:
+            GPIO.output(GPIO_FRONTLED10, False)
+            
+        # Get distance data from rear module and process
+        os.system("bash -c 'ping -w 1 umbc.edu'")
+        os.system("bash -c 'ping -w 1 umbc.edu'")
+        rightDistance = randint(1,2000) # rand for now later will be obtained from rear
+        leftDistance = randint(1,2000) # also a temporary rand
+        
+        if rightDistance <= 120:
+            GPIO.output(GPIO_LEDSRIGHT, True)
+        else:
+            GPIO.output(GPIO_LEDSRIGHT, False)
+        if leftDistance <= 120:
+            GPIO.output(GPIO_LEDSLEFT, True)
+        else:
+            GPIO.output(GPIO_LEDSLEFT, False)
+        
+        time.sleep(0.25) # sleep for a tenth of a second
+
+    except KeyboardInterrupt:
+    # User pressed CTRL-C
+    # Reset GPIO settings
+    GPIO.cleanup()
+
+# ----------------------------------------------------------------------
+# Main function that will run all front module processes
+# ----------------------------------------------------------------------
+def main():
+    cameraProcess = Process(target=pictureDisplay) # may need to add ", args=()" inside parenthesis
+    sensorsProcess = Process(target=updateLEDs) # may need to add ", args=()" inside parenthesis
+    cameraProcess.start()
+    sensorsProcess.start()
+    cameraProcess.join()
+    sensorsProcess.join()
+    
+main()
--- RearModule.py
+++ RearModule.py
@@ -11,7 +11,7 @@
 # http://www.raspberrypi-spy.co.uk/tag/ultrasonic/
 #
 # Author : Matt Hawkins
-# Date   : 16/10/2016
+# Date     : 16/10/2016
 # -----------------------
 
 # -----------------------
@@ -27,8 +27,6 @@ import os
 # Define GPIO to use on Pi
 def GPIO_TRIGGER1 = 23
 def GPIO_ECHO1    = 24
-def GPIO_LED1     = 17
-def GPIO_LED2     = 27
 def GPIO_TRIGGER2 = 5
 def GPIO_ECHO2    = 6
 
@@ -40,176 +38,158 @@ def speedSound = 13500 # in/s
 # measure1 takes a measurement from the first sensor
 # ---------------------------------------------------
 def measure1():
-  # This function measures a distance
-  GPIO.output(GPIO_TRIGGER1, True)
-  # Wait 10us
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER1, False)
-  start = time.time()
-  
-  while GPIO.input(GPIO_ECHO1)==0:
+    # This function measures a distance
+    GPIO.output(GPIO_TRIGGER1, True)
+    # Wait 10us
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER1, False)
+    start = time.time()
+    
+    while GPIO.input(GPIO_ECHO1)==0:
     start = time.time()
 
-  while GPIO.input(GPIO_ECHO1)==1:
+    while GPIO.input(GPIO_ECHO1)==1:
     stop = time.time()
 
-  elapsed = stop-start
-  distance = (elapsed * speedSound)/2
+    elapsed = stop-start
+    distance = (elapsed * speedSound)/2
 
-  return distance
+    return distance
 
 # ---------------------------------------------------
 # measure_average1 finds the average of 3 measurements
 # ---------------------------------------------------
 def measure_average1():
-  # This function takes 3 measurements and
-  # returns the average.
+    # This function takes 3 measurements and
+    # returns the average.
 
-  distance1=measure1()
-  time.sleep(0.1)
-  distance2=measure1()
-  time.sleep(0.1)
-  distance3=measure1()
-  distance = distance1 + distance2 + distance3
-  distance = distance / 3
-  return distance
+    distance1=measure1()
+    time.sleep(0.1)
+    distance2=measure1()
+    time.sleep(0.1)
+    distance3=measure1()
+    distance = distance1 + distance2 + distance3
+    distance = distance / 3
+    return distance
  
 # ---------------------------------------------------
 # measure2 takes a measurement from the second sensor
 # --------------------------------------------------- 
 def measure2():
-  # This function measures a distance
-  GPIO.output(GPIO_TRIGGER2, True)
-  # Wait 10us
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER2, False)
-  start = time.time()
-  
-  while GPIO.input(GPIO_ECHO2)==0:
+    # This function measures a distance
+    GPIO.output(GPIO_TRIGGER2, True)
+    # Wait 10us
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER2, False)
+    start = time.time()
+    
+    while GPIO.input(GPIO_ECHO2)==0:
     start = time.time()
 
-  while GPIO.input(GPIO_ECHO2)==1:
+    while GPIO.input(GPIO_ECHO2)==1:
     stop = time.time()
 
-  elapsed = stop-start
-  distance = (elapsed * speedSound)/2
+    elapsed = stop-start
+    distance = (elapsed * speedSound)/2
 
-  return distance
+    return distance
 
 # ---------------------------------------------------
 # measure_average2 finds the average of 3 measurements
 # ---------------------------------------------------
 def measure_average2():
-  # This function takes 3 measurements and
-  # returns the average.
+    # This function takes 3 measurements and
+    # returns the average.
 
-  distance1=measure2()
-  time.sleep(0.1)
-  distance2=measure2()
-  time.sleep(0.1)
-  distance3=measure2()
-  distance = distance1 + distance2 + distance3
-  distance = distance / 3
-  return distance
+    distance1=measure2()
+    time.sleep(0.1)
+    distance2=measure2()
+    time.sleep(0.1)
+    distance3=measure2()
+    distance = distance1 + distance2 + distance3
+    distance = distance / 3
+    return distance
 
 # ----------------------------------------------------
 # Main Script for Side Sensors
 # ----------------------------------------------------
 def SideSensors():
-  # Use BCM GPIO references
-  # instead of physical pin numbers
-  GPIO.setmode(GPIO.BCM)
+    # Use BCM GPIO references
+    # instead of physical pin numbers
+    GPIO.setmode(GPIO.BCM)
 
-  print("Ultrasonic Measurement")
+    print("Ultrasonic Measurement")
 
-  # Set pins as output and input 
-  GPIO.setup(GPIO_TRIGGER1,GPIO.OUT)  # Trigger 1
-  GPIO.setup(GPIO_ECHO1,GPIO.IN)      # Echo 1
-  GPIO.setup(GPIO_LED1,GPIO.OUT)      # LED 1
-  GPIO.setup(GPIO_LED2,GPIO.OUT)      # LED 2
-  GPIO.setup(GPIO_TRIGGER2,GPIO.OUT)  # Tigger Trigger 2 
-  GPIO.setup(GPIO_ECHO2,GPIO.IN)      # ECHO 2
+    # Set pins as output and input 
+    GPIO.setup(GPIO_TRIGGER1,GPIO.OUT)    # Trigger 1
+    GPIO.setup(GPIO_ECHO1,GPIO.IN)        # Echo 1
+    GPIO.setup(GPIO_TRIGGER2,GPIO.OUT)    # Tigger Trigger 2 
+    GPIO.setup(GPIO_ECHO2,GPIO.IN)        # ECHO 2
 
-  # Set trigger to False (Low)
-  GPIO.output(GPIO_TRIGGER1, False)
-  GPIO.output(GPIO_LED1, False)
-  GPIO.output(GPIO_TRIGGER2, False)
-  GPIO.output(GPIO_LED2, False)
+    # Set trigger to False (Low)
+    GPIO.output(GPIO_TRIGGER1, False)
+    GPIO.output(GPIO_TRIGGER2, False)
 
-  # Allow module to settle
-  time.sleep(0.5)
+    # Allow module to settle
+    time.sleep(0.5)
 
-  # Wrap main content in a try block so we can
-  # catch the user pressing CTRL-C and run the
-  # GPIO cleanup function. This will also prevent
-  # the user seeing lots of unnecessary error
-  # messages.
-  try:
+    # Wrap main content in a try block so we can
+    # catch the user pressing CTRL-C and run the
+    # GPIO cleanup function. This will also prevent
+    # the user seeing lots of unnecessary error
+    # messages.
+    try:
     print("Distance Sensor 1\tDistance Sensor 2") 
     while True:
-      distance1 = measure_average1()
-	  os.system("bash -c 'ping -w 1 umbc.edu'")
-	  #ping umbc.edu
-      # printing distance for know will eventually delete this
-      # print("Distance Sensor 1: %d inches" %distance)
-      # if there is something less than 4 ft away from sensor
-      if distance1 <= 48:
-        # Turn on led 1
-        GPIO.output(GPIO_LED1, True)
-      else:
-        GPIO.output(GPIO_LED1, False)
+        distance1 = measure_average1()
+        os.system("bash -c 'ping -w 1 umbc.edu'")
+        #ping umbc.edu
     
-      distance2 = measure_average2()
-	  os.system("bash -c 'ping -w 1 umbc.edu'")
-	  #ping -w 1 umbc.edu
- 
-      # print("Distance Sensor 2: %d inches" % distance)
-      if distance2 <= 48:
-        GPIO.output(GPIO_LED2, True)
-      else:
-        GPIO.output(GPIO_LED2, False)
+        distance2 = measure_average2()
+        os.system("bash -c 'ping -w 1 umbc.edu'")
+        #ping -w 1 umbc.edu
     
-      print(int(distance1), "inches\t\t", int(distance2), "inches")
-      time.sleep(0.5)
+        print(int(distance1), "inches\t\t", int(distance2), "inches")
+        time.sleep(0.5)
 
-  except KeyboardInterrupt:
+    except KeyboardInterrupt:
     # User pressed CTRL-C
     # Reset GPIO settings
     GPIO.cleanup()
-	
+    
 # -----------------------------------------------------------------
 # Main Script for taking pictures with the camera
 # -----------------------------------------------------------------
 def TakePictures():
-  NUM_PICTURES = 1200 # Will take pics for 10 minutes
-  SLEEP_TIME = 50/100 
+    NUM_PICTURES = 1200 # Will take pics for 10 minutes
+    SLEEP_TIME = 50/100 
 
-  camera = PiCamera()
+    camera = PiCamera()
 
-  for i in range(NUM_PICTURES):
+    for i in range(NUM_PICTURES):
     camera.start_preview()
     time.sleep(SLEEP_TIME) # Sleep for SLEEP_TIME seconds
     #Used to take a still picture
     picture = ('test.jpg') # Made this just test.jpg so we dont overuse memory
     camera.capture(picture)
-	os.system("bash -c 'ping -w 1 umbc.edu'")
-	#ping umbc.edu
+    os.system("bash -c 'ping -w 1 umbc.edu'")
+    #ping umbc.edu
 
     camera.stop_preview()
-	
-	# --------------------------------------------
-	# Add code here to send image to front module
-	# --------------------------------------------
-	
+    
+    # --------------------------------------------
+    # Add code here to send image to front module
+    # --------------------------------------------
+    
 # ----------------------------------------------------------------------
 # Main function that will run all rear module processes
 # ----------------------------------------------------------------------
 def main():
-  cameraProcess = Process(target=TakePictures) # may need to add ", args=()" inside parenthesis
-  sensorsProcess = Process(target=SideSensors) # may need to add ", args=()" inside parenthesis
-  cameraProcess.start()
-  sensorsProcess.start()
-  cameraProcess.join()
-  sensorsProcess.join()
-  
+    cameraProcess = Process(target=TakePictures) # may need to add ", args=()" inside parenthesis
+    sensorsProcess = Process(target=SideSensors) # may need to add ", args=()" inside parenthesis
+    cameraProcess.start()
+    sensorsProcess.start()
+    cameraProcess.join()
+    sensorsProcess.join()
+    
 main()
